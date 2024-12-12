001f83d89616c7acc2f958bf8d44a1afefeaa2ff
Inoscopedjfk02496 <95453182+Inoscopedjfk02496@users.noreply.github.com>
2024-12-12 12:38:01 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,2 @@
-
-    var head = document.getElementsByTagName('head')[0];
-    var script = document.createElement('script');
-    script.type = 'text/javascript';
-    script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
-
-    script.onreadystatechange = handler;
-    script.onload = handler;
-
-    head.appendChild(script);
 $(body).append("<div style = 'width:100px; height:100px; background:red;'></div>");
 console.log("c");
